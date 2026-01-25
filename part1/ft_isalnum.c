01e705dfc1d7126f678aa2c862559a1aa8ddbf37
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-25 18:09:05 +0300

    added main for test and other option func

--- part1/ft_isalnum.c
+++ part1/ft_isalnum.c
@@ -1,13 +1,20 @@
 int ft_isalnum(int c)
 {
-    return ((c >= 'a' && c <= 'z')
-         || (c >= 'A' && c <= 'Z')
-         || (c >= '0' && c <= '9'));
+    return (ft_isalpha(c) || ft_isdigit(c));
 }
 
 /*
 int ft_isalnum(int c)
 {
-    return (ft_isalpha(c) || ft_isdigit(c));
+    return ((c >= 'a' && c <= 'z')
+         || (c >= 'A' && c <= 'Z')
+         || (c >= '0' && c <= '9'));
 }
+*/
+
+/*
+//0, 5, a,Z, 12, /, @
+int main() {
+    printf("%d", ft_isalnum());
+} 
 */
