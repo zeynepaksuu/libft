d9e30ada50f941471368e86af43d6e8f2629453c
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-26 21:33:04 +0300

    main func was fixed and added new test case

---
+++ part1/ft_strchr.c
@@ -0,0 +1,56 @@
+#include <string.h>
+char *ft_strchr(const char *s, int c)
+{
+    unsigned int i;
+    char charc;
+    
+    charc = (char) c;
+    i = 0;
+    
+    while (s[i])
+    {
+        if (s[i] == charc) {
+            return ((char*) &s[i]);
+        }
+        i++;
+    }
+
+    if(s[i] == charc)
+    {
+        return ((char *) &s[i]);
+    }
+    
+    return (NULL);
+}
+
+/*
+int main()
+{
+    char dize[] = "deneme";
+    char *sonuc;
+
+    sonuc = ft_strchr(dize, 'h');
+    if (sonuc != NULL) 
+    {
+        printf("%s\n", sonuc);
+    }
+    else 
+    {
+        printf("xn");
+    }
+    sonuc = ft_strchr(dize, 'z');
+    if (sonuc != NULL) 
+    {
+        printf("%s\n", sonuc);
+    }
+    else 
+    {
+        printf("x\n");
+    }
+    sonuc = ft_strchr(dize, '\0');
+    if (sonuc != NULL)
+    {
+        printf("null");
+    }
+}
+*/
