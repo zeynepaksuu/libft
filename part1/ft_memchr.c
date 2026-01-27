df8e1a9445bd04acfb915ea061544331809b384a
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-27 18:15:35 +0300

    fixed code and main func

--- part1/ft_memchr.c
+++ part1/ft_memchr.c
@@ -1,46 +1,38 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char	*str;
-	unsigned char	uc;
+	unsigned char	*way;
+	unsigned char	who;
 	size_t			i;
 
-	str = (unsigned char *)s;
-	uc = (unsigned char)c;
+	way = (unsigned char *)s;
+	who = (unsigned char)c;
 	i = 0;
 	while (i < n)
 	{
-		if (str[i] == uc)
+		if (way[i] == who)
 		{
-			return ((void *)(str + i));
+			return ((void *)(way + i));
 		}
 		i++;
 	}
 	return (NULL);
 }
 
-
 /*
-#include <stdio.h>
 int	main(void)
 {
 	char	*str = "zaksu";
 	char	*res1 = ft_memchr(str, 'm', 3);
-
-	//string
 	if (res1)
-		printf("Bulunan karakterden itibaren: %s\n", res1);
+		printf("%s\n", res1);
 	else
-		printf("Karakter bulunamadi.\n");
-
-	// 0
+		printf("x\n");
 	char	data[] = {'a', 'b', '\0', 'c', 'd', 'e'};
 	char	*res2 = ft_memchr(data, 'd', 6);
-
-	//null iceren
 	if (res2)
-		printf("'d' karakteri bulundu! Adres farki: %ld\n", res2 - data);
+		printf("%ld\n", res2 - data);
 	else
-		printf("Karakter bulunamadi.\n");
+		printf("x\n");
 	return (0);
 }
 */
