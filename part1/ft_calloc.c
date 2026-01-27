d31f54c10f96ddc89220e78d2e3dd7d1181d07a6
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-27 19:49:41 +0300

    added for all test case main func

--- part1/ft_calloc.c
+++ part1/ft_calloc.c
@@ -5,14 +5,35 @@ void	*ft_calloc(size_t count, size_t size)
 
 	if (count != 0 && (count * size) / count != size)
 		return (NULL);
-
 	total = count * size;
 	ptr = malloc(total);
-
 	if (!ptr)
 		return (NULL);
-
 	ft_bzero(ptr, total);
 
 	return (ptr);
 }
+
+/*
+int main()
+{
+    size_t count = 5;
+    int *dizi;
+    size_t i;
+
+    dizi = (int *)ft_calloc(count, sizeof(int));
+
+    if (dizi != NULL)
+    {
+        printf("ok\n");
+        for (i = 0; i < count; i++)
+        {
+            printf("dizi[%zu] = %d\n", i, dizi[i]); 
+        }
+        free(dizi);
+    }
+    else
+        printf("x\n");
+    return (0);
+}
+*/
