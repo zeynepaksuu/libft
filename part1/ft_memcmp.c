fc1e1561b94311af9276d84464b9de9e3e436ff3
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-27 18:26:07 +0300

    code fixed and main func added

--- part1/ft_memcmp.c
+++ part1/ft_memcmp.c
@@ -1,17 +1,17 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	*p1;
-	unsigned char	*p2;
+	unsigned char	*c1;
+	unsigned char	*c2;
 	size_t			i;
 
-	p1 = (unsigned char *)s1;
-	p2 = (unsigned char *)s2;
+	c1 = (unsigned char *)s1;
+	c2 = (unsigned char *)s2;
 	i = 0;
 	while (i < n)
 	{
-		if (p1[i] != p2[i])
+		if (c1[i] != c2[i])
 		{
-			return (p1[i] - p2[i]);
+			return (c1[i] - c2[i]);
 		}
 		i++;
 	}
@@ -19,21 +19,14 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 }
 
 /*
-#include <stdio.h>
-#include <string.h>
-
 int	main(void)
 {
 	char *s1 = "abcdef";
 	char *s2 = "abcdez";
-
 	printf("%d\n", ft_memcmp(s1, s2, 6));
 	printf("%d\n", ft_memcmp(s1, s2, 5)); 
-	
 	int n1[] = {1, 2, 0, 4};
 	int n2[] = {1, 2, 0, 5};
 	printf("%d\n", ft_memcmp(n1, n2, sizeof(int) * 4));
-	
-	return (0);
 }
 */
