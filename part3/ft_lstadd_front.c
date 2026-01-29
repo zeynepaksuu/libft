4bbe63d0e22d57b26376e23eac4a62c6e0fd121f
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-30 00:19:57 +0300

    main func added

--- part3/ft_lstadd_front.c
+++ part3/ft_lstadd_front.c
@@ -6,3 +6,33 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 		*lst = new;
 	}
 }
+
+/*
+typedef struct s_list
+{
+    void            *content;
+    struct s_list   *next;
+} t_list;
+
+int main(void)
+{
+    t_list  *liste = NULL;
+    t_list  *yeni;
+
+    yeni = ft_lstnew("selam");
+    ft_lstadd_front(&liste, yeni);
+
+    yeni = ft_lstnew("len");
+    ft_lstadd_front(&liste, yeni);
+
+    t_list *gecici = liste;
+    while (gecici)
+    {
+        printf("%s -> ", (char *)gecici->content);
+        gecici = gecici->next;
+    }
+    printf("NULL\n");
+
+    return (0);
+}
+*/
