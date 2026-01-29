f79ddfb523ef1b72e6f5dc56f1ba601fe3b66693
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-29 23:50:57 +0300

    added new main func, it will test in python tutor for see the recursive algorithm

--- part2/ft_putnbr_fd.c
+++ part2/ft_putnbr_fd.c
@@ -16,3 +16,20 @@ void	ft_putnbr_fd(int n, int fd)
 	else
 		ft_putchar_fd(nb + '0', fd);
 }
+
+/*
+int main(void)
+{
+	ft_putnbr_fd(42, 1);
+	write(1, "\n", 1);
+
+	ft_putnbr_fd(-1234, 1);
+	write(1, "\n", 1);
+
+	ft_putnbr_fd(-2147483648, 1);
+	write(1, "\n", 1);
+
+	ft_putnbr_fd(0, 1);
+	write(1, "\n", 1);
+}
+*/
