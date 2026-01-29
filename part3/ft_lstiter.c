38a03999933f6c14441a91812d804aab0d470575
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-30 01:30:48 +0300

    added main fnc

--- part3/ft_lstiter.c
+++ part3/ft_lstiter.c
@@ -9,3 +9,24 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 		lst = lst->next;
 	}
 }
+
+/*
+typedef struct s_list {
+    void            *content;
+    struct s_list   *next;
+} t_list;
+
+//contenti yazdıralım
+void print_content(void *content) {
+    printf("deneme: %s\n", (char *)content);
+}
+
+int main(void) {
+    t_list *liste = ft_lstnew("kaju");
+    ft_lstadd_back(&liste, ft_lstnew("badem"));
+    ft_lstadd_back(&liste, ft_lstnew("fındık"));
+
+    ft_lstiter(liste, print_content);
+    return (0);
+}
+*/
