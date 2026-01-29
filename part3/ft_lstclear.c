8499fe37ad859f3bfad9bd252b0cce885e46ab36
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-30 01:26:08 +0300

    main func added

--- part3/ft_lstclear.c
+++ part3/ft_lstclear.c
@@ -12,3 +12,27 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	}
 	*lst = NULL;
 }
+
+/*
+typedef struct s_list {
+    void            *content;
+    struct s_list   *next;
+} t_list;
+
+void del_func(void *content) {
+    free(content);
+}
+
+int main(void) {
+    t_list *bas = ft_lstnew(strdup("Vagon 1"));
+    ft_lstadd_back(&bas, ft_lstnew(strdup("Vagon 2")));
+    ft_lstadd_back(&bas, ft_lstnew(strdup("Vagon 3")));
+
+    printf("önce: %p\n", bas);
+    ft_lstclear(&bas, del_func);
+
+    printf("sonra: %p\n", bas); // \0
+
+    return (0);
+}
+*/
