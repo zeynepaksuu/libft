7a52f2b36af8bbcf4c48d2b4ce643cfd506fd808
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-29 23:08:41 +0300

    added main func and changed syntax

--- part2/ft_split.c
+++ part2/ft_split.c
@@ -15,24 +15,24 @@ static int	count_words(const char *s, char c)
 	return (count);
 }
 
-static char	**free_all(char **res, int i)
+static char	**free_all(char **howm, int i)
 {
 	while (i >= 0)
-		free(res[i--]);
-	free(res);
+		free(howm[i--]);
+	free(howm);
 	return (NULL);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**res;
+	char	**howm;
 	int		i;
 	int		len;
 
 	if (!s)
 		return (NULL);
-	res = malloc(sizeof(char *) * (count_words(s, c) + 1));
-	if (!res)
+	howm = malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (!howm)
 		return (NULL);
 	i = 0;
 	while (*s)
@@ -44,12 +44,40 @@ char	**ft_split(char const *s, char c)
 			len = 0;
 			while (s[len] && s[len] != c)
 				len++;
-			res[i++] = ft_substr(s, 0, len);
-			if (!res[i - 1])
-				return (free_all(res, i - 2));
+			howm[i++] = ft_substr(s, 0, len);
+			if (!howm[i - 1])
+				return (free_all(howm, i - 2));
 			s += len;
 		}
 	}
-	res[i] = NULL;
-	return (res);
+	howm[i] = NULL;
+	return (howm);
 }
+
+
+/*
+int main(void)
+{
+    char    *str = "  --deneme-test-bir-ki-uc  ";
+    char    **tablo;
+    int     i;
+
+    i = 0;
+    tablo = ft_split(str, '-');
+    
+    if (!tablo)
+    {
+        printf("x\n");
+        return (1);
+    }
+
+    while (tablo[i] != NULL)
+    {
+        printf("%d: [%s]\n", i, tablo[i]);
+        free(tablo[i]);
+        i++;
+    }
+    free(tablo);
+    return (0);
+}
+*/
