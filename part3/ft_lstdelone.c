26f22e11511fd23fe75d128a78d8abdd0ab2980c
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-30 00:39:17 +0300

    it will be test

--- part3/ft_lstdelone.c
+++ part3/ft_lstdelone.c
@@ -7,3 +7,39 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 
 	free(lst);
 }
+
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct s_list {
+    void            *content;
+    struct s_list   *next;
+} t_list;
+
+//standart free
+void del_content(void *content) {
+    free(content);
+}
+
+void ft_lstdelone(t_list *lst, void (*del)(void *));
+
+int main(void) {
+    t_list *dugum;
+    char *str = strdup("deneme");
+
+    dugum = malloc(sizeof(t_list));
+    dugum->content = str;
+    dugum->next = NULL;
+
+    printf("önce: %s\n", (char *)dugum->content);
+
+    ft_lstdelone(dugum, del_content);
+
+    printf("silindi\n");
+    // artık dügüme ulasmaya calısmak hata olcak ama denemek lazım
+
+    return (0);
+}
+*/
