2f9a660c34f6d6b54e8979f0758201bb9a7d7b75
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-29 23:40:05 +0300

    added main func and null control

--- part2/ft_putstr_fd.c
+++ part2/ft_putstr_fd.c
@@ -4,3 +4,23 @@ void	ft_putstr_fd(char *s, int fd)
 		return ;
 	write(fd, s, ft_strlen(s));
 }
+
+/*
+int main(void)
+{
+	char *mesaj = "deneme";
+	char *hata = "HATA";
+
+	// 1
+	ft_putstr_fd(mesaj, 1);
+	write(1, "\n", 1);
+
+	// 2
+	// ekranda aynı görünebilir ama sisteme farklı kaydediliyor
+	ft_putstr_fd(hata, 2);
+	write(1, "\n", 1);
+
+	// 3(nullicin)
+	ft_putstr_fd(NULL, 1);
+}
+*/
