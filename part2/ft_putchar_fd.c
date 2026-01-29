a972b5b9f5e73d595af068794286ef5ab9d7ebf0
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-29 23:36:23 +0300

    added new main func and deleted 3. option (real file)

--- part2/ft_putchar_fd.c
+++ part2/ft_putchar_fd.c
@@ -2,3 +2,16 @@ void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
+
+/*
+int main(void)
+{
+	// 1- ekran
+	ft_putchar_fd('A', 1);
+	ft_putchar_fd('\n', 1);
+
+	// 2- error kanal
+	ft_putchar_fd('H', 2);
+	ft_putchar_fd('\n', 2);
+}
+*/
