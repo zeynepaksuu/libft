3ec99c3b84e95aa5be23d49d83040f315df10a7e
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-29 23:45:11 +0300

    added new main func, maybe added putstr example for see the diffrent

--- part2/ft_putendl_fd.c
+++ part2/ft_putendl_fd.c
@@ -5,3 +5,15 @@ void	ft_putendl_fd(char *s, int fd)
 	ft_putstr_fd(s, fd);
 	write(fd, "\n", 1);
 }
+
+/*
+int main(void)
+{
+	char *str = "deneme";
+	
+	ft_putendl_fd(str, 1);
+	ft_putendl_fd(str, 1);
+
+	return (0);
+}
+*/
