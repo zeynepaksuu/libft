283cd7e98c13ce3abc97504909009ee4ed60bb4b
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-29 23:30:13 +0300

    added new main and my_func

--- part2/ft_striteri.c
+++ part2/ft_striteri.c
@@ -11,3 +11,24 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 		i++;
 	}
 }
+
+/*
+void my_iter_func(unsigned int i, char *c)
+{
+    // indeks tek ise harfi buyutcez
+    if (i % 2 != 0 && (*c >= 'a' && *c <= 'z'))
+        *c = *c - 32;
+}
+int main(void)
+{
+    // salt okunur bir dize (char *str = "abc") kullanılamazmıs
+    // striteri dizeyi degistirmeye calisiyor. bellekte yeni bir dize accaz
+    char metin[] = "merhaba"; 
+
+    printf("girilen : %s\n", metin);
+    
+    ft_striteri(metin, my_iter_func);
+    
+    printf("cıktı : %s\n", metin);
+}
+*/
