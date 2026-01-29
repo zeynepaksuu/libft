51f87eb90b59332cb90af084bc70b06dd7a099fb
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-30 00:15:20 +0300

    added main func but it will test the t_list struct

--- part3/ft_lstnew.c
+++ part3/ft_lstnew.c
@@ -9,3 +9,33 @@ t_list	*ft_lstnew(void *content)
 	new_elem->next = NULL;
 	return (new_elem);
 }
+
+/*
+
+//
+typedef struct s_list
+{
+    void            *content;
+    struct s_list   *next;
+} t_list;
+
+int main(void)
+{
+    t_list  *dugum;
+    char    *metin = "deneme";
+
+    dugum = ft_lstnew(metin);
+
+    if (dugum)
+    {
+        //(char *) kullanıoz çünkü content void*
+        printf("Icerik: %s\n", (char *)dugum->content);
+        
+        if (dugum->next == NULL)
+            printf("null atandı\n");
+
+        free(dugum);
+    }
+    return (0);
+}
+*/
