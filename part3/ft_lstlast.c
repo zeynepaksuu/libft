5a968b856b3315d178fca8ef550177388c908599
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-30 00:27:52 +0300

    added main func

--- part3/ft_lstlast.c
+++ part3/ft_lstlast.c
@@ -6,3 +6,29 @@ t_list	*ft_lstlast(t_list *lst)
 		lst = lst->next;
 	return (lst);
 }
+
+/*
+typedef struct s_list
+{
+    void            *content;
+    struct s_list   *next;
+} t_list;
+
+int main(void)
+{
+    t_list  *liste = NULL;
+
+    ft_lstadd_front(&liste, ft_lstnew("sondu bas"));
+    ft_lstadd_front(&liste, ft_lstnew("orta"));
+    ft_lstadd_front(&liste, ft_lstnew("bas"));
+
+    t_list *son = ft_lstlast(liste);
+
+    if (son)
+        printf("son veri : %s\n", (char *)son->content);
+    else
+        printf("bos liste\n");
+
+    return (0);
+}
+*/
