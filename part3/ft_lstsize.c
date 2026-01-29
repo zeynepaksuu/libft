8dc2eba2227b8add26f30558be902afcfe4e61c5
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-30 00:24:24 +0300

    added main func

--- part3/ft_lstsize.c
+++ part3/ft_lstsize.c
@@ -10,3 +10,27 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (size);
 }
+
+/*
+typedef struct s_list
+{
+    void            *content;
+    struct s_list   *next;
+} t_list;
+
+int main(void)
+{
+    t_list  *liste = NULL;
+
+    // eleman ekliyoruz
+    ft_lstadd_front(&liste, ft_lstnew("Düğüm 1"));
+    ft_lstadd_front(&liste, ft_lstnew("Düğüm 2"));
+    ft_lstadd_front(&liste, ft_lstnew("Düğüm 3"));
+
+    int result = ft_lstsize(liste);
+    
+    printf("%d\n", count); // 3
+
+    return (0);
+}
+*/
