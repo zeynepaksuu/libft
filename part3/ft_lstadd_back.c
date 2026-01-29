16372f2d3c5eb97faed289baa8f9cfe6347bd693
Zeynep Aksu <zeynepgspor@gmail.com>
2026-01-30 00:32:30 +0300

    added new main func but it will test

--- part3/ft_lstadd_back.c
+++ part3/ft_lstadd_back.c
@@ -12,3 +12,32 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	last = ft_lstlast(*lst);
 	last->next = new;
 }
+
+/*
+typedef struct s_list
+{
+    void            *content;
+    struct s_list   *next;
+} t_list;
+
+int main(void)
+{
+    t_list  *liste = NULL;
+
+    // listeye yeni eleman
+    ft_lstadd_back(&liste, ft_lstnew("Vagon 1"));
+
+    ft_lstadd_back(&liste, ft_lstnew("Vagon 2"));
+    ft_lstadd_back(&liste, ft_lstnew("Vagon 3"));
+
+    t_list *temp = liste;
+    while (temp)
+    {
+        printf("%s -> ", (char *)temp->content);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+
+    return (0);
+}
+*/
